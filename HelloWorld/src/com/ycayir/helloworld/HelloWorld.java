298505153bf14bcdd7657bc797f981e20fbcf4b5
Yalcin Cayir <yalcincayir@gmail.com>
2015-12-22 23:16:15 -0500

    Initial commit.

---
+++ HelloWorld/src/com/ycayir/helloworld/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.ycayir.helloworld;
+
+/**
+ * @author ycayir
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
